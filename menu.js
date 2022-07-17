64b72eb3bdd212f6a2eb09e3b52c664172d5746a
Oluizn <antonioluiz_neto@hotmail.com>
2022-07-17 16:28:00 -0300

    att

--- menu.js
+++
@@ -1,8 +0,0 @@
-function menushow() {
-    var menumobile = document.getElementById('.hidden-menu');
-    if (menumobile.classList.contains('open')) {
-        menumobile.classList.remove('open');
-    } else {
-        menumobile.classList.add('open')
-    }
-}
